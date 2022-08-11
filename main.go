32803d7cb150453b42dd66ab1951336f68a30e5b
bos <a@bos-info.com>
2022-08-11 20:30:50 +0300

    some fixes after linters

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const (
 	countOfRecords = 3
 	fName          = "data.json"
 
-	//Errors Description
+	// Errors Description.
 	emptyDeptSt    = "empty departure station"
 	badDeptSt      = "bad departure station input"
 	emptyArrSt     = "empty arrival station"
@@ -45,31 +45,41 @@ func main() {
 	fmt.Println("----------- Інформаційна система УкрЗалізниця 3000 -----------")
 	fmt.Println("Оберіть станцію відправлення")
 	_, err := fmt.Scanf("%v", &departureStation)
+
 	if err != nil {
 		log.Fatal(emptyDeptSt)
 	}
+
 	departureStation = strings.Trim(departureStation, "\"")
 	fmt.Println("Оберіть станцію прибуття")
+
 	_, err = fmt.Scanf("%v", &arrivalStation)
+
 	if err != nil {
 		log.Fatal(emptyArrSt)
 	}
+
 	arrivalStation = strings.Trim(arrivalStation, "\"")
 	fmt.Println("Оберіть критерій, по якому сортувати результат")
 	_, err = fmt.Scanf("%v", &criteria)
+
 	if err != nil {
 		log.Fatal(unsCriteria)
 	}
+
 	criteria = strings.Trim(criteria, "\"")
 	result, err := FindTrains(departureStation, arrivalStation, criteria)
+
 	if err != nil {
 		fmt.Println("Х-Х-Х-Х-Х-Х-Х-Х-Х-Х-Х- ПОМИЛКА -Х-Х-Х-Х-Х-Х-Х-Х-Х-Х-Х")
 		log.Fatal(err)
 	}
 	fmt.Println("---------------------- Результат пошуку ----------------------")
+
 	if result == nil {
 		fmt.Println("Відсутні потяги між вказанами станціями.")
 	}
+
 	for _, v := range result {
 		fmt.Printf("TrainID: %d DepartureStationID: %d ArrivalStationID: %d  Price: %0.2f ArrivalTime: %v DepartureTime: %v\n",
 			v.TrainID, v.DepartureStationID, v.ArrivalStationID, v.Price, v.ArrivalTime, v.DepartureTime)
@@ -77,35 +87,40 @@ func main() {
 	fmt.Println("--------------------------------------------------------------")
 }
 
-// FindTrains шукаємо потяги що задовольняють введеним умовам користувача
+// FindTrains шукаємо потяги що задовольняють введеним умовам користувача.
 func FindTrains(departureStation, arrivalStation, criteria string) (Trains, error) {
-	//записуємо підготовлені дані в trainsSlice
+	// записуємо підготовлені дані в trainsSlice
 	trainsSlice := readFile()
+
 	var depStationInt, arrStationInt int
-	//Перевіряємо валідність станції відправлення
+
+	// Перевіряємо валідність станції відправлення
 	if len(departureStation) == 0 {
 		return nil, errors.New(emptyDeptSt)
 	}
+
 	depStationInt, errDeptSt := strconv.Atoi(departureStation)
 	if errDeptSt != nil || depStationInt < 0 {
 		return nil, errors.New(badDeptSt)
 	}
-	//Перевіряємо валідність станції прибуття
+	// Перевіряємо валідність станції прибуття
 	if len(arrivalStation) == 0 {
 		return nil, errors.New(emptyArrSt)
 	}
+
 	arrStationInt, errArrSt := strconv.Atoi(arrivalStation)
 	if errArrSt != nil || arrStationInt < 0 {
 		return nil, errors.New(badArrSt)
 	}
+
 	var result Trains
-	//Додаємо всі потяги, що задовольняють умові курсування між станціями
+	// Додаємо всі потяги, що задовольняють умові курсування між станціями
 	for _, v := range trainsSlice {
 		if v.DepartureStationID == depStationInt && v.ArrivalStationID == arrStationInt {
 			result = append(result, v)
 		}
 	}
-	//Перевіряємо правильність введення критерію сортування та сортуємо по ньому.
+	// Перевіряємо правильність введення критерію сортування та сортуємо по ньому.
 	switch criteria {
 	case "price":
 		sort.SliceStable(result, func(i, j int) bool { return result[i].Price < result[j].Price })
@@ -114,17 +129,19 @@ func FindTrains(departureStation, arrivalStation, criteria string) (Trains, erro
 	case "departure-time":
 		sort.SliceStable(result, func(i, j int) bool { return result[i].DepartureTime.Before(result[j].DepartureTime) })
 	default:
-		//якщо введено невалідний критерій повертаємо помилку
+		// якщо введено невалідний критерій повертаємо помилку
 		return nil, errors.New(unsCriteria)
 	}
-	//якщо результатів більше ніж вимагається за умовою задачі
+
+	// якщо результатів більше ніж вимагається за умовою задачі
 	if len(result) > countOfRecords {
 		return result[:countOfRecords], nil // маєте повернути правильні значення
 	}
-	//якщо по вказаним критеріям потягів не знайдено повертаємо nil
+	// якщо по вказаним критеріям потягів не знайдено повертаємо nil
 	if len(result) == 0 {
 		return nil, nil
 	}
+
 	return result, nil
 }
 
@@ -137,19 +154,22 @@ func readFile() Trains {
 	// оброюблюємо закриття файлу
 	defer closeFile(file)
 	byteValue, _ := io.ReadAll(file)
+
 	var uz Trains
-	//Додаткова перевірка валідності структури JSON(не вимагалась за умовою задачі)
+
+	// Додаткова перевірка валідності структури JSON(не вимагалась за умовою задачі)
 	if isValid := json.Valid(byteValue); !isValid {
 		log.Fatal(invalidJSON)
 	}
-	//Обробка помилки при обробці даних з файлу
+	// Обробка помилки при обробці даних з файлу
 	if err = json.Unmarshal(byteValue, &uz); err != nil {
 		log.Fatal(err)
 	}
+
 	return uz
 }
 
-// closeFile функція закриває файл
+// closeFile функція закриває файл.
 func closeFile(f *os.File) {
 	err := f.Close()
 	if err != nil {
@@ -158,13 +178,15 @@ func closeFile(f *os.File) {
 	}
 }
 
-// UnmarshalJSON описуємо метод анмаршалінгу JSON для Train
+// UnmarshalJSON описуємо метод анмаршалінгу JSON для Train.
 func (tr *Train) UnmarshalJSON(j []byte) error {
 	var rawData map[string]any
 	err := json.Unmarshal(j, &rawData)
+
 	if err != nil {
 		return errors.New(unmarshalError)
 	}
+
 	for k, v := range rawData {
 		switch k {
 		case "trainId":
@@ -225,5 +247,6 @@ func (tr *Train) UnmarshalJSON(j []byte) error {
 			}
 		}
 	}
+
 	return nil
 }
